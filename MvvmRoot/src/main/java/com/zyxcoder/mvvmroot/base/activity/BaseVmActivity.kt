bc14e75525e284aab5f4349343a28cb5bcf21594
zhangyuxiang <zyx_coder@163.com>
2024-03-21 17:27:48 +0800

    ViewModel创建方式修改

--- MvvmRoot/src/main/java/com/zyxcoder/mvvmroot/base/activity/BaseVmActivity.kt
+++ MvvmRoot/src/main/java/com/zyxcoder/mvvmroot/base/activity/BaseVmActivity.kt
@@ -58,7 +58,7 @@ abstract class BaseVmActivity<VM : BaseViewModel> : AppCompatActivity() {
      * 创建viewModel
      */
     private fun createViewModel(): VM {
-        return ViewModelProvider(this)[getVmClazz(this)]
+        return ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application))[getVmClazz(this)]
     }
 
     /**
