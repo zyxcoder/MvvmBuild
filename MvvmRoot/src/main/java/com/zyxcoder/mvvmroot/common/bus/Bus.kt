cce03ba3c47eff312de0bfccae157caef91e59b7
zhangyuxiang <zyx_coder@163.com>
2025-07-01 17:13:42 +0800

    跨进程发消息

--- MvvmRoot/src/main/java/com/zyxcoder/mvvmroot/common/bus/Bus.kt
+++ MvvmRoot/src/main/java/com/zyxcoder/mvvmroot/common/bus/Bus.kt
@@ -9,6 +9,10 @@ object Bus {
     inline fun <reified T> post(channel: String, value: T) =
         LiveEventBus.get(channel, T::class.java).post(value)
 
+    // 跨进程发送（支持序列化类型）
+    inline fun <reified T> postAcrossProcess(channel: String, value: T) =
+        LiveEventBus.get(channel, T::class.java).postAcrossProcess(value)
+
     inline fun <reified T> observe(
         channel: String,
         owner: LifecycleOwner,
@@ -43,4 +47,11 @@ object Bus {
         value: T,
         clazz: Class<T>
     ) = LiveEventBus.get(channel, clazz).post(value)
+
+    // Java兼容的跨进程发送
+    fun <T> postAcrossProcessForJava(
+        channel: String,
+        value: T,
+        clazz: Class<T>
+    ) = LiveEventBus.get(channel, clazz).postAcrossProcess(value)
 }
